638f8301e73a72b349971243741e83a8610292cd
Marc Meadows <marcm@meadowface.net>
2016-03-01 18:27:58 -0500

    Reduce symbols exposed by moving functions inside of the prompt function.  Add tests.

--- bash-prompt-vcs.bash
+++ bash-prompt-vcs.bash
@@ -1,107 +1,3 @@
-function bpvcs_reset_state() {
-    vcs=""
-    changed=0
-    untracked=0
-    staged=0
-    branch=""
-}
-
-function bpvcs_git_state() {
-    bpvcs_reset_state
-
-    local line
-    while IFS= read -r line ; do
-        if [ "${line:0:2}" = "xx" ]; then
-            return 0
-        fi
-
-        if [ "${line:2:1}" != " " ]; then
-            return 0;
-        fi
-
-        # https://git-scm.com/docs/git-status
-        local x=${line:0:1}
-        local y=${line:1:1}
-
-        if [ "$x$y" = "??" ]; then
-            ((untracked++))
-        elif [ "$x$y" = "##" ]; then
-            branch=${line:3}
-        elif [ "$y" = "M" ]; then
-            ((changed++))
-        else
-            ((staged++))
-        fi
-        #NOTE: -z would complicate parsing dramatically because of renames
-        #      and it's not worth it since git escapes pretty well.
-    done < <(git status --porcelain --branch 2>/dev/null || echo -e "xx $?")
-
-    vcs="git"
-    return 1
-}
-
-function bpvcs_hg_state() {
-    bpvcs_reset_state
-
-    local line
-    while IFS= read -r line ; do
-        if [ "${line:0:2}" = "xx" ]; then
-            return 0
-        fi
-
-        # commit messages start with " " so it won't ever match a field.
-        local field="${line/:*/}"
-        if [ "$field" = "branch" ]; then
-            branch="${line/*: /}"
-        elif [ "$field" = "commit" ]; then
-            local item
-            while IFS= read -r -d ',' item; do
-                item="${item## }"
-                item="${item%% (*)}"
-                local count="${item/ */}"
-                local kind="${item/* /}"
-                case $kind in
-                    modified|added|renamed) ((changed += count)) ;;
-                    unknown)  ((untracked += count)) ;;
-                esac
-            done  <<< "${line/*?: /},"  #NOTE: need the trailing comma for read
-        fi
-    done < <(hg summary 2>/dev/null || echo -e "xx: $?")
-
-    # branch: should *always* be present, if not assume bad output
-    if [ -z "$branch" ]; then
-        return 0
-    fi
-
-    vcs="hg"
-    return 1
-}
-
-function bpvcs_svn_state() {
-    bpvcs_reset_state
-
-    local line
-    while IFS= read -r line ; do
-        if [ "${line:0:22}" = "svn: warning: W155007:" ]; then
-            return 0
-        fi
-
-        case "${line:0:1}" in
-            A|M|R) ((changed++)) ;;
-            \?)    ((untracked++)) ;;
-            " ")   if [ "${line:1:1}" = "M" ]; then ((changed++)); fi ;;
-            # The following are all valid but we don't use.  Use them to
-            # be able to detect bad output in the *) case.
-            C|D|I|X|!|~) ;;
-            *) return 0 ;;
-        esac
-    #svn status doesn't return anything but 0, but keep the mechanism in place
-    done < <(svn status --depth immediates 2>&1 || echo -e "xx: $?")
-
-    vcs="svn"
-    return 1
-}
-
 BPVCS_UNTRACKED_INDICATOR="…"
 BPVCS_CHANGED_INDICATOR="△"
 BPVCS_STAGED_INDICATOR="●"
@@ -113,7 +9,123 @@ BPVCS_SVN_COLOR="\033[0;35m"
 BPVCS_COLORS=1
 
 function bpvcs_bash_prompt() {
+    local vcs
+    local changed
+    local untracked
+    local staged
+    local branch
+
+    function bpvcs_reset_state() {
+        vcs=""
+        changed=0
+        untracked=0
+        staged=0
+        branch=""
+    }
+
+    function bpvcs_git_state() {
+        bpvcs_reset_state
+
+        local line
+        while IFS= read -r line ; do
+            if [ "${line:0:2}" = "xx" ]; then
+                return 0
+            fi
+
+            if [ "${line:2:1}" != " " ]; then
+                return 0;
+            fi
+
+            # https://git-scm.com/docs/git-status
+            local x=${line:0:1}
+            local y=${line:1:1}
+
+            if [ "$x$y" = "??" ]; then
+                ((untracked++))
+            elif [ "$x$y" = "##" ]; then
+                branch=${line:3}
+            elif [ "$y" = "M" ]; then
+                ((changed++))
+            else
+                ((staged++))
+            fi
+            #NOTE: -z would complicate parsing dramatically because of renames
+            #      and it's not worth it since git escapes pretty well.
+        done < <(git status --porcelain --branch 2>/dev/null || echo -e "xx $?")
+
+        vcs="git"
+        return 1
+    }
+
+    function bpvcs_hg_state() {
+        bpvcs_reset_state
+
+        local line
+        while IFS= read -r line ; do
+            if [ "${line:0:2}" = "xx" ]; then
+                return 0
+            fi
+
+            # commit messages start with " " so it won't ever match a field.
+            local field="${line/:*/}"
+            if [ "$field" = "branch" ]; then
+                branch="${line/*: /}"
+            elif [ "$field" = "commit" ]; then
+                local item
+                while IFS= read -r -d ',' item; do
+                    item="${item## }"
+                    item="${item%% (*)}"
+                    local count="${item/ */}"
+                    local kind="${item/* /}"
+                    case $kind in
+                        modified|added|renamed) ((changed += count)) ;;
+                        unknown)  ((untracked += count)) ;;
+                    esac
+                done  <<< "${line/*?: /},"  #NOTE: need the trailing comma for read
+            fi
+        done < <(hg summary 2>/dev/null || echo -e "xx: $?")
+
+        # branch: should *always* be present, if not assume bad output
+        if [ -z "$branch" ]; then
+            return 0
+        fi
+
+        vcs="hg"
+        return 1
+    }
+
+    function bpvcs_svn_state() {
+        bpvcs_reset_state
+
+        local line
+        while IFS= read -r line ; do
+            if [ "${line:0:22}" = "svn: warning: W155007:" ]; then
+                return 0
+            fi
+
+            case "${line:0:1}" in
+                A|M|R) ((changed++)) ;;
+                \?)    ((untracked++)) ;;
+                " ")   if [ "${line:1:1}" = "M" ]; then ((changed++)); fi ;;
+                # The following are all valid but we don't use.  Use them to
+                # be able to detect bad output in the *) case.
+                C|D|I|X|!|~) ;;
+                *) return 0 ;;
+            esac
+        #svn status doesn't return anything but 0, but keep the mechanism in place
+        done < <(svn status --depth immediates 2>&1 || echo -e "xx: $?")
+
+        vcs="svn"
+        return 1
+    }
+
     bpvcs_git_state && bpvcs_hg_state && bpvcs_svn_state
+
+    # functions are always global, so unset these so they don't leak.
+    unset bpvcs_reset_state
+    unset bpvcs_git_state
+    unset bpvcs_hg_state
+    unset bpvcs_svn_state
 
     #TODO: have an errors flag too incase vc returns something unparsable.
     local vcstate=""
